678a215bb3439f93de3085492f17d6872194a65f
Mamed766 <mehemmeddemirli57@gmail.com>
2025-04-10 14:44:15 +0400

    chore: recent expenses screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,7 @@
 import { Text } from "react-native";
 
 function ManageExpenses() {
-  return <Text>Manege Expenses Screen</Text>;
+  return <Text>Manege Expense Screen</Text>;
 }
 
 export default ManageExpenses;
