70d8c620c2e817c0a17bc2cf3caa7e9035af0e51
Mamed766 <mehemmeddemirli57@gmail.com>
2025-04-11 12:41:23 +0400

    feat: Manage Expense isEditing

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,29 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { View } from "react-native";
+import { GlobalStyles } from "../constants/styles";
+import IconButton from "../components/UI/IconButton";
 
-function ManageExpense() {
-  return <Text>Manege Expense Screen</Text>;
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  return (
+    <View>
+      {isEditing && (
+        <IconButton
+          name="trash"
+          color={GlobalStyles.colors.error500}
+          size={36}
+        />
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
